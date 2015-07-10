c442307acadf635e52b2151b447266ccd4f521b1
Shweta Bhandare <bhandare@colorado.edu>
2015-07-10 07:10:58 -0600

    Added more files.

---
+++ src/SeqGenUtils.py
@@ -0,0 +1,10 @@
+from random import choice
+def weightedchoice(items): # this doesn't require the numbers to add up to 100
+	return choice("".join(x * y for x, y in items))
+
+def GetRandomSequence(seqLen):
+	seq=""
+	for count in range(length): 
+		seq+=weightedchoice([("C", 10], ("G", 20), ("A", 40"), ("T", 30)]);
+	return seq;
+
