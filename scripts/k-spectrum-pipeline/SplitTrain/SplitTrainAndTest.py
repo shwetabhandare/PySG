db5928975d92414a47f4cad8f45baf35edf63356
Shweta Bhandare <bhandare@colorado.edu>
2015-08-25 13:31:30 -0600

    Adding split algorithm.

---
+++ scripts/k-spectrum-pipeline/SplitTrain/SplitTrainAndTest.py
@@ -0,0 +1,82 @@
+import fasta
+import sys
+import random
+import re
+import os
+
+
+def getAllData(dataFile): 
+	allData = []
+	for record in fasta.fasta_itr(dataFile):						#Create parallel arrays for negative sequence/headers
+		sequence = re.sub('[()\'\']', '', record.sequence.strip());
+		header = re.sub('[()\'\']', '', record.header.strip());
+		allData.append([header, sequence]);
+
+	return allData;
+
+def getTrainIds(allData, numTrain):
+	trainIdx = [];
+	for item in allData:
+		index = random.randint(0, len(allData) - 1);
+		rc = index in trainIdx;
+		while (rc != False):
+			index = random.randint(0, len(allData) - 1);
+			rc = index in trainIdx;
+		if (len(trainIdx) < numTrain):
+			trainIdx.append(index)
+	return trainIdx;
+
+
+def getTestIds(trainIds, allData):
+	testIds = []
+	for i in range(0, len(allData)):
+		index = random.randint(0, len(allData) - 1);
+		rc = index in trainIds;
+		while (rc != False):
+			index = random.randint(0, len(allData) - 1);
+			rc = index in trainIds;
+		rc1 = index in testIds;
+		if (rc == False and rc1 == False):
+			testIds.append(index);
+
+	return testIds;
+
+def getTrainAndTestData(allData, trainPercent, testPercent):
+	testData = [];
+	trainData = [];
+	numTrain = (len(allData) * trainPercent)/100;	
+	trainIds = getTrainIds(allData, numTrain);
+	testIds = getTestIds(trainIds, allData);
+	numAdded = 0;
+	for i in range(0, len(trainIds)):
+		indexToAdd = trainIds[i];
+		trainData.append(allData[indexToAdd]);
+	for i in range(0, len(testIds)):
+		indexToAdd = testIds[i];
+		testData.append(allData[indexToAdd]);
+	return trainData, testData;
+			
+
+def writeDataToFile(data, outputFile):
+	outFile = open(outputFile , "w")
+	for item in data:
+		outFile.write(">")
+		outFile.write(item[0]);
+		outFile.write("\n")
+		outFile.write(item[1]);
+		outFile.write("\n")
+	outFile.close();
+
+# main starts here
+#Prepare and open files
+dataFile = sys.argv[1]
+trainPercent = int(sys.argv[2]);
+testPercent = 100 - trainPercent;
+trainFileName = sys.argv[3]
+testFileName = sys.argv[4]
+
+allData = getAllData(dataFile);
+trainData, testData = getTrainAndTestData(allData, trainPercent, testPercent);
+print len(trainData), len(testData)
+writeDataToFile(trainData, trainFileName);
+writeDataToFile(testData, testFileName);
