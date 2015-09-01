76791766de81aa65412739307f468d0fc9b8116f
Shweta Bhandare <shweta.bhandare@logrhythm.com>
2015-09-01 14:11:49 -0600

    Update seq_counter.py

--- scripts/k-spectrum-pipeline/seq_counter.py
+++ scripts/k-spectrum-pipeline/seq_counter.py
@@ -4,51 +4,61 @@ import sys
 import os
 
 #Taking in filenames as arguments, +ve and then -ve
-filename_plus = sys.argv[1]
-filename_minus = sys.argv[2]
 
-#Open files as read-only
-file_plus = open(filename_plus, "r")
-file_minus = open(filename_minus, "r")
 
-#Creating "text" of the files to use
-plus_text = file_plus.read()
-minus_text = file_minus.read()
 
-plus_count = 0
-minus_count = 0
 
-#Split and search for the character '>', which denotes new sequence
-plus_split = plus_text.split()
-minus_split = minus_text.split()
+def CreateCombinedFile(posFile, negFile)
+## This needs to be completed.
 
-for word in plus_split:
-	if '>' in word:
-		plus_count = plus_count +1
+	filename_plus = posFile;
+	filename_minus = negFile;
+	
+	#Open files as read-only
+	file_plus = open(filename_plus, "r")
+	file_minus = open(filename_minus, "r")
+	
+	#Creating "text" of the files to use
+	plus_text = file_plus.read()
+	minus_text = file_minus.read()
+	
+	plus_count = 0
+	minus_count = 0
+	
+	#Split and search for the character '>', which denotes new sequence
+	plus_split = plus_text.split()
+	minus_split = minus_text.split()
+	
+	for word in plus_split:
+		if '>' in word:
+			plus_count = plus_count +1
+	
+	for word in minus_split:
+		if '>' in word:
+			minus_count = minus_count+1
+	
+	#Creating output file and writing other files to it
+	just_plus_basename = os.path.basename(filename_plus)
+	just_minus_basename = os.path.basename(filename_minus)
+	print just_plus_basename
+	just_plus = os.path.splitext(just_plus_basename)[0]		#splitting the filename from extension and only taking the filename
+	just_minus = os.path.splitext(just_minus_basename)[0]		#splitting the filename from extension and only taking the filename
+	#just_plus = os.path.splitext(filename_plus)[0]		#splitting the filename from extension and only taking the filename
+	#just_minus = os.path.splitext(filename_minus)[0]
+	pcount = str(plus_count)
+	mcount = str(minus_count)
+	filename_output = ""+just_plus+"_"+just_minus+"_"+pcount+"_"+mcount+"_CompleteSet.txt"	#creating the file name for the output file
+	outputfile = open(filename_output, "w")	
+	outputfile.write(plus_text)
+	outputfile.write(minus_text)
+	
+	#Closing files
+	file_plus.close()
+	file_minus.close()
+	outputfile.close()
+	#would return here but I am just printing
+	print(filename_output, plus_count, minus_count)
 
-for word in minus_split:
-	if '>' in word:
-		minus_count = minus_count+1
+if __name__ == "__main__":
+	CreateCombinedFile(posFile, negFile);
 
-#Creating output file and writing other files to it
-just_plus_basename = os.path.basename(filename_plus)
-just_minus_basename = os.path.basename(filename_minus)
-print just_plus_basename
-just_plus = os.path.splitext(just_plus_basename)[0]		#splitting the filename from extension and only taking the filename
-just_minus = os.path.splitext(just_minus_basename)[0]		#splitting the filename from extension and only taking the filename
-#just_plus = os.path.splitext(filename_plus)[0]		#splitting the filename from extension and only taking the filename
-#just_minus = os.path.splitext(filename_minus)[0]
-pcount = str(plus_count)
-mcount = str(minus_count)
-filename_output = ""+just_plus+"_"+just_minus+"_"+pcount+"_"+mcount+"_CompleteSet.txt"	#creating the file name for the output file
-outputfile = open(filename_output, "w")	
-outputfile.write(plus_text)
-outputfile.write(minus_text)
-
-#Closing files
-file_plus.close()
-file_minus.close()
-outputfile.close()
-
-#would return here but I am just printing
-print(filename_output, plus_count, minus_count)
