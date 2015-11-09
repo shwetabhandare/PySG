d4a878c053b10dfe5bbcf36b6933ab3c08ac0108
Shweta Bhandare <shweta.bhandare@logrhythm.com>
2015-11-08 20:56:17 -0700

    HuR Train/Test results (In Domain).

---
+++ scripts/k-spectrum-pipeline/createModelTTP.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Lines starting with #SBATCH are treated by bash as comments, but interpreted by slurm
+# as arguments.
+
+#
+# Set the name of the job
+#SBATCH -J createHuRModel
+
+#
+# Set a walltime for the job. The time format is HH:MM:SS - In this case we run for 5 minutes.
+
+#SBATCH --time=48:00:00
+
+#
+# Select one node
+#
+
+#SBATCH -N 1
+# Select one task per node (similar to one processor per node)
+#SBATCH --ntasks-per-node 1
+
+# Set output file name with job number
+#SBATCH -o testjob-%j.out
+# Use the janus-lon QOS
+#SBATCH --qos janus-long
+
+# The following commands will be executed when this script is run.
+
+echo The job has begun
+make -f Makefile.GenerateCombinedFilesTTP && make -f Makefile.TTP_Train && make -f Makefile.TTP_Test && make -f Makefile.SaveResultsTTPTrainTest
