ee2dddc7eac6a6194d14fa476ba230ae4876395b
Shweta Bhandare <shweta.bhandare@logrhythm.com>
2015-12-11 14:17:56 -0700

    Updated data to make sure the names match what they really are. Created fasta files.

--- scripts/k-spectrum-pipeline/GenerateFlankingRegions.py
+++ scripts/k-spectrum-pipeline/GenerateFlankingRegions.py
@@ -6,6 +6,8 @@ from fasta import *
 import operator
 from itertools import *
 import math
+from collections import defaultdict;
+from collections import OrderedDict
 
 def ReadConfigFile():
 	confMap = {}
@@ -28,15 +30,13 @@ def fasta_read(file_name):
 def CreateKmerDict(kmerFile):
 	with open(kmerFile, 'rb') as csvFile:
 		reader = csv.reader(csvFile, delimiter=',');
-		numFeatures = 0;
 		kmerDict = dict()
-		for row in reader:
-			if numFeatures <= 30:
-				print row[0]
-				featureScore = float(row[0])
-				featureKmer = row[1]
-				kmerDict[featureKmer] = featureScore;
-				numFeatures = numFeatures + 1;
+		for idx, row in enumerate(reader):
+			#if idx <=10000:
+			featureScore = float(row[0])
+			featureKmer = row[1]
+			kmerDict[featureKmer] = featureScore;
+	kmerDict = defaultdict(lambda: -1, kmerDict)
 	return kmerDict;
 
 def CreateFlankingRegion(sequence, kmer, flankStart, kmerIndex, flankEnd):
@@ -79,15 +79,15 @@ def GetKmerCounts(kmerDict, seqDict):
 				kmerCountDict[kmer] = kmerCountDict[kmer] + kmerCount;
 			else:
 				kmerCountDict[kmer] = kmerCount;	
-
+	kmerCountDict = defaultdict(lambda: -1, kmerCountDict)
 	return kmerCountDict;
 
 	
 def GetSortedKmerDict(kmerCountDict):
 
-	sorted_kmerCountDict = [ (v,k) for k,v in kmerCountDict.iteritems() ]
-	sorted_kmerCountDict.sort(reverse=True)
-
+	sorted_kmerCountDict = OrderedDict(sorted(kmerCountDict.items(), key=lambda x: x[1]))
+	
+	print type(sorted_kmerCountDict);
 	return sorted_kmerCountDict;
 
 def WriteTopLogLikelihoodKmers(topLikelyKmers, topKmersFile):
@@ -151,35 +151,36 @@ def ComputeLogLikelihood(posSeq, negSeq, kmerFile, topKmersFile):
 	posKmerCountDict = GetKmerCounts(kmerDict, posSeqDict);
 	negKmerCountDict = GetKmerCounts(kmerDict, negSeqDict);
 
-	sortedPosKmerCountList = GetSortedKmerDict(posKmerCountDict)
-	sortedNegKmerCountList = GetSortedKmerDict(negKmerCountDict)
-
+	sortedPosKmerCountDict = GetSortedKmerDict(posKmerCountDict)
+	sortedNegKmerCountDict = GetSortedKmerDict(negKmerCountDict)
 
 	LogLikeHood = dict();
 
 	totalPosSeq = len(posSeqDict)
 	totalNegSeq = len(negSeqDict)
 
-	totalPosKmers = len(sortedPosKmerCountList)
-	totalNegKmers = len(sortedNegKmerCountList)
+	totalPosKmers = len(sortedPosKmerCountDict)
+	totalNegKmers = len(sortedNegKmerCountDict)
 
-	for pos_item in sortedPosKmerCountList:
-		for neg_item in sortedNegKmerCountList:
-			kmer = pos_item[1]
-			if kmer == neg_item[1]:
-				positiveCount = pos_item[0]
-				negativeCount = neg_item[0]
-				print positiveCount, negativeCount
-				posParam = LaplaceSmoothing(totalPosSeq, totalPosKmers, positiveCount)
-				negParam = LaplaceSmoothing(totalNegSeq, totalNegKmers, negativeCount)
+	for posKmer, posKmerCount in sortedPosKmerCountDict.iteritems():
+		if posKmer in sortedNegKmerCountDict:
+			negativeCount = 0;
+		else:
+			negativeCount = sortedNegKmerCountDict[posKmer]
 
-				if positiveCount == 0:
-					LogLikeHood[kmer] = 0;
-				elif negativeCount == 0:
-					print "Could not find ", kmer, " in negative set."
-					LogLikeHood[kmer] = 0;
-				else:
-					LogLikeHood[kmer] = math.log10( float(posParam)/ float(negParam))
+		positiveCount = posKmerCount
+				#neg_item[0]
+		print positiveCount, negativeCount
+		posParam = LaplaceSmoothing(totalPosSeq, totalPosKmers, positiveCount)
+		negParam = LaplaceSmoothing(totalNegSeq, totalNegKmers, negativeCount)
+
+		if positiveCount == 0:
+			LogLikeHood[posKmer] = float("-inf");
+		elif negativeCount == 0:
+			print "Could not find ", posKmer, " in negative set."
+			LogLikeHood[posKmer] = float("-inf")
+		else:
+			LogLikeHood[posKmer] = math.log10( float(posParam)/ float(negParam))
 
 	sortedLogLikeHood =  [ (v,k) for k,v in LogLikeHood.iteritems() ]
 	sortedLogLikeHood.sort(reverse=True)
