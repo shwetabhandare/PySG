a04a7bbb4591e56e66d25af353defdfc5d6dbe17
Shweta Bhandare <shweta.bhandare@logrhythm.com>
2016-01-28 16:03:04 -0700

    Updated code. Adding results for TTP
    Former-commit-id: 9b0f242cc494127cd11e835f31a3309b9ba51d31

--- src/SeqGenMotif.py
+++ src/SeqGenMotif.py
@@ -7,27 +7,25 @@ def ReadPWMFile(filename):
 	pwm = []
 	f = open(filename)
 	lines = f.readlines()
-	fields = lines[0].split()
-	print "fields:", fields[4]
-	#print "fields:", fields
-	name = fields[4]
-	print "Name: ", name
-	
-	vals_A = lines[1].rstrip().split('\t')
-	vals_C = lines[2].rstrip().split('\t')
-	vals_T = lines[3].rstrip().split('\t')
-	vals_G = lines[4].rstrip().split('\t')
-
-	for i in range(0,len(vals_A)):
-		vals = [float(vals_A[i]), float(vals_C[i]), float(vals_G[i]), float(vals_T[i])]
-		pwm.append(vals)
-	
 	f.close()
 	
-	#print "PWM:"
-	#for v in pwm:
-	#	print "\t",v
+	line1 = lines[1].rstrip().split('\t')
+	line2 = lines[2].rstrip().split('\t')
+	line3 = lines[3].rstrip().split('\t')
+	line4 = lines[4].rstrip().split('\t')
+
+	print line1, line2, line3, line4
+	pwm.append(line1)
+	pwm.append(line2)
+	pwm.append(line3)
+	pwm.append(line4)
+
+	
+	print "PWM:"
+	for v in pwm:
+		print "\t",v
 		
+	name = "dreme";
 	return name, pwm
 
 def MakePWMMotif(filename):
@@ -36,11 +34,12 @@ def MakePWMMotif(filename):
 	name, pwm = ReadPWMFile(filename)
 	
 	print "Building motif:", name
-	#m = MotifTools.toDict(pwm)
-	#print m
-	#motif = MotifTools.Motif_from_counts(m)
-	#print vals_A, vals_C, vals_T, vals_G
-	motif = MotifTools.Motif_from_text("TGASTCA")
+	m = MotifTools.toDict(pwm)
+	print m
+	#motif = MotifTools.Motif_from_ll(m)
+	#A 0.307 C 0.141 G 0.060 T 0.493
+	#motif = MotifTools.Motif_from_counts(m, beta=0.01, bg={'A': 0.307, 'C': 0.141, 'G': 0.060, 'T': 0.493})
+	motif = MotifTools.Motif_from_counts(m)
 	motif.source = name
 	
 	print "Motif:", motif.source
@@ -55,3 +54,4 @@ if __name__ == "__main__":
 	motifFile = sys.argv[1]
 	print "Motif file: ", motifFile
 	name, pwm = MakePWMMotif(motifFile)
+	#name, pwn = ReadPWMFile(motifFile)
