b177439317356d95936db3e235df8a8a50da0650
Shweta Bhandare <shweta.bhandare@logrhythm.com>
2016-02-21 15:09:47 -0700

    Updated generateYaml to generate Yaml files for NoSignal sequences with input as 3UTR files.
    Former-commit-id: 131e549fa7ad3f4f7f013fa6965042c07e538599
    Former-commit-id: 4b24559ff2db860ed9290f24df0184fab9cc7b46

--- src/generateYaml.py
+++ src/generateYaml.py
@@ -1,29 +1,10 @@
 import yaml
 
+#numSeq = [1000, 2000, 3000, 4000, 5000]
+#seqLen = [200, 300, 400, 500, 600, 700, 800, 900, 1000]
 
-#seqMinLen = [19]
-#seqMaxLen = [343]
-#aPercent = [35]
-#tPercent = [35]
-#gPercent = [15]
-#cPercent = [15]
-#MotifType = ['']
-#MotifType = ['HuR']
-#aPercent = [10, 20, 30, 40]
-#tPercent = [10, 20, 30, 40]
-#gPercent = [10, 20, 30, 40]
-#cPercent = [10, 20,s 30, 40]
-#aPercent = [10, 20, 30, 40]
-#tPercent = [10, 20, 30, 40]
-#gPercent = [10, 20, 30, 40]
-#cPercent = [10, 20, 30, 40]
-#MotifType = ['HuR', 'TTP', 'Generated']
-seqMinLen = [50, 100]
-seqMaxLen = [105, 150]
-aPercent = [10, 20, 30, 40]
-tPercent = [10, 20, 30, 40]
-gPercent = [10, 20, 30, 40]
-cPercent = [10, 20, 30, 40]
+numSeq = [1000, 2000, 3000, 4000, 5000]
+seqLen = [200, 300, 400, 500, 600, 700, 800, 900, 1000]
 # generate A, T, G, C using s = np.random.dirichlet((0.1,0.1,0.1,0.1),1)
 # expect sparse distribution, there is no apriori information.
 # tp = np.random.dirichlet((0.1,0.1,0.1,0.1),1)[0]
@@ -45,86 +26,35 @@ cPercent = [10, 20, 30, 40]
 # when gamma is large, the ATGC percentages will be similar, and when its smaller they will differ more.
 # here gammma = 100.
 
-MotifType = ['HuR', 'TTP', 'Generated']
-HuRMotif = ['ATTTA', 'CTTTTTC']
-TtpMotif = ['TTATTTATT']
-NumMotifs = [1]
-DistanceMotifs = [20, 70, 100]
-MotifLocation = ['random', -5, 10]
 
-numberSeq = 3642;
-
-aPercent = 30;
-tPercent = 30;
-gPercent = 20;
-cPercent = 20;
-
-def generateYaml(idx, location, minVal, maxVal, numberSeq, aPercent, tPercent, gPercent, cPercent, motifType, actualMotif, numMotifs, distance):
-	if numMotifs == 1:
-		distance = 0;
-
-	if motifType != "":
-
-		data = dict(
-		 sequence = dict (
-			  minLen = minVal,
-			  maxLen = maxVal,
-			  numSeq = numberSeq,
-			  A = aPercent,
-			  T = tPercent,
-			  G = gPercent,
-			  C = cPercent,
-		  ),
-		motif = dict (
-			type = motifType,
-			length = len(actualMotif),
-			numMotifs = numMotifs,
-			distance = distance,
-			location = 'random',
-			motif = actualMotif,
-		 )
-		)
-	else:
-		data = dict(
-		 sequence = dict (
-			  minLen = minVal,
-			  maxLen = maxVal,
-			  numSeq = numberSeq,
-			  A = aPercent,
-			  T = tPercent,
-			  G = gPercent,
-			  C = cPercent,
+def generateYaml(location, numberSeq, seqLength, inpFastaFile):
+	outFileName = "NoSignal_" + str(numberSeq) + "_" + str(seqLength)
+	print outFileName
+	data = dict(
+	 sequence = dict (
+		nosignal = dict(
+		  seqLen = seqLength,
+		  numSeq = numberSeq,
+		  fastaFile = inpFastaFile,
+		  outFastaFile = location + "/" + outFileName + ".fa",
 		  )
-		)		
-	yamlFileName = location + "/seq_" + str(idx) + ".yml";
+		)
+	)
+	yamlFileName = location + "/" + outFileName + ".yml";
 	print "Data: ", data
 	with open(yamlFileName, 'w') as outfile:
 		 outfile.write( yaml.dump(data, default_flow_style=True) )
 
 ## Program stats here.
 def CreateConfFiles(location):
-	for idx, minVal in enumerate(seqMinLen):
-		for maxVal in seqMaxLen:
-			for numMotifs in NumMotifs:
-				for distance in DistanceMotifs:
-					for motifType in MotifType:
-						print "MotifType: ", motifType
-						if motifType == 'HuR':
-							for hurMotif in HuRMotif:
-								idx = str(minVal) + "_" + str(maxVal) + "_" + hurMotif;
-								generateYaml(idx, location, minVal, maxVal, numberSeq, aPercent, tPercent,
-												 gPercent, cPercent, motifType, hurMotif, numMotifs, distance);
-						elif motifType == 'TTP':
-							for ttpMotif in TtpMotif:
-								idx = str(minVal) + "_" + str(maxVal) + "_" + ttpMotif;
-								generateYaml(idx, location, minVal, maxVal, numberSeq, aPercent, tPercent,
-												 gPercent, cPercent, motifType, ttpMotif, numMotifs, distance);
-						elif motifType == 'Generated':
-							idx = str(minVal) + "_" + str(maxVal);
-							generateYaml(idx, location, minVal, maxVal, numberSeq, aPercent, tPercent,
-												 gPercent, cPercent, motifType, "AAATTTGGGCCC", numMotifs, distance);
+	inpFastaFile = "/projects/bhandare/workspace/scripts/NegFileCreator/3UTR_transcripts_Human.txt"
+	for numSeqIdx, i in enumerate(numSeq):
+		for numSeqLenIdx, j in enumerate(seqLen):
+			print i, j
+			generateYaml(location, i, j, inpFastaFile);
 
-						else: 
-							generateYaml(idx, location, minVal, maxVal, numberSeq, aPercent, tPercent,
-										 gPercent, cPercent, "", "", 0, 0);
-#CreateConfFiles("/tmp");
+
+if __name__ == "__main__":
+	import sys
+	location = sys.argv[1]
+	CreateConfFiles(location)
