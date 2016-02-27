8b979306b9b213f009632a28397adf5b427b9cea
Shweta Bhandare <shweta.bhandare@logrhythm.com>
2016-02-27 05:58:01 -0700

    Removed files not wanted from k-spectrum.
    Former-commit-id: 39011cfe1fcf640ab9ced5a2bd5f2781a8ccf9c1
    Former-commit-id: a18978ba7ffdec7d374776366471b1f4d2c2da71

--- src/main.py
+++ src/main.py
@@ -1,35 +1,26 @@
-import Conf;
-import SeqGen;
-from Conf import *;
-from SeqGen import *;
-import generateYaml;
 from generateYaml import *;
 from SeqGenUtils import *;
 import sys;
 
 directory = sys.argv[1]
-CreateConfFiles(directory);
-
-
-
+CreateConfFiles(directory, 'shuffle');
 
 
 for confFile in findFiles(directory, '*.yml'):
 
-	conf = Conf(confFile);
-
-	seqGen = SeqGen(conf);
-	filename = os.path.splitext(os.path.basename(confFile))[0]
-	posFastaFile = directory + "/" + filename + "_pos.fa";
-	seqGen.SetPosFileName(posFastaFile);
-
-	negFastaFile = directory + "/" + filename + "_neg.fa";
-
-	seqGen.SetNegFileName(negFastaFile)
-
-	seqGen.GenerateRandomSequences("negative", 0);
-	seqGen.GenerateRandomSequences("positive", 1);
-	#seqGen.embedMotifInSequence();
+	print confFile;
+#	seqGen = SeqGen(conf);
+#	filename = os.path.splitext(os.path.basename(confFile))[0]
+#	posFastaFile = directory + "/" + filename + "_pos.fa";
+#	seqGen.SetPosFileName(posFastaFile);
+#
+#	negFastaFile = directory + "/" + filename + "_neg.fa";
+#
+#	seqGen.SetNegFileName(negFastaFile)
+#
+#	seqGen.GenerateRandomSequences("negative", 0);
+#	seqGen.GenerateRandomSequences("positive", 1);
+#	#seqGen.embedMotifInSequence();
 
 	# print "Positive Set: "
 	# for seq in seqGen.GetPositiveSet():
@@ -39,5 +30,5 @@ for confFile in findFiles(directory, '*.yml'):
 	# for seq in seqGen.GetNegativeSet():
 	# 	print seq;
 
-	seqGen.writePositiveFile();
-	seqGen.writeNegativeFile();
+#	seqGen.writePositiveFile();
+#	seqGen.writeNegativeFile();
