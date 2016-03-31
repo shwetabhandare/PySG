44c6e7e758d22bddf1a55c196419310d6ea6b29c
Shweta Bhandare <shweta.bhandare@logrhythm.com>
2016-03-31 11:24:02 -0600

    Updated unit tests, added function to normalize counts.

--- src/splitKmerInDict.py
+++ src/splitKmerInDict.py
@@ -27,7 +27,9 @@ def getInitializedLists(numKeys):
 
 	return aList, tList, gList, cList;
 
-def getUpdatedCounts(aList, tList, gList, cList, kmerDictList):
+def getUpdatedCounts(maxKmerLen, kmerDictList):
+	aList, tList, gList, cList = getInitializedLists(maxKmerLen);
+
 	for kDict in kmerDictList:
 		for key, value in kDict.iteritems():
 			if value == 'A':
@@ -39,11 +41,39 @@ def getUpdatedCounts(aList, tList, gList, cList, kmerDictList):
 			elif value == 'G':
 				gList[key] = gList[key] + 1;
 
+	return aList, tList, gList, cList;
 
+def createNormalizedLists(aList, tList, gList, cList, numKmers):
+	aList[:] = [round(float(x)/numKmers, 3) for x in aList]
+	tList[:] = [round(float(x)/numKmers, 3) for x in tList]
+	gList[:] = [round(float(x)/numKmers, 3) for x in gList]
+	cList[:] = [round(float(x)/numKmers, 3) for x in cList]
+
+	return aList, tList, gList, cList;
+
+def createPwm(aList, tList, gList, cList):
+
+	pwm = [4]
+
+	pwm[0] = tList;
+	pwm[1] = gList;
+	pwm[2] = cList;
+	pwm[3] = aList;
+
+
+	return pwm;
 if __name__ == "__main__":
 	import sys
 	kmerFile = sys.argv[1]
 	kmerDict = parseKspectrum.FindKspectrumKmers(kmerFile)
 	kmerList = kmerDict.keys();
 	maxKmerLen, kmerDictList = createKmerDictList(kmerList);
-	aList, tList, gList, cList = getInitializedLists(maxKmerLen);
+	aList, tList, gList, cList = getUpdatedCounts(maxKmerLen, kmerDictList);
+	numKmers = len(kmerList)
+	aList, tList, gList, cList = createNormalizedLists(aList, tList, gList, cList, numKmers);
+	print "A List: ", aList
+	print "C List: ", cList
+	print "G List: ", gList
+	print "T List: ", tList
+	pwm = createPwm(aList, tList, gList, cList)
+	print pwm
--- src/test_create_count_list.py
+++ src/test_create_count_list.py
@@ -29,3 +29,46 @@ class TestCreateCountList(unittest.TestCase):
 		shared_items = set(kmerDict2.items()) & set(kmerDictList[1].items())
 		self.assertEqual(len(shared_items), len(kmerDict2))
 
+	def testGetUpdateCounts(self):
+		kmerList = ['TTAATATTT', 'TTTTTATTT']
+		maxKmerLen, kmerDictList = splitKmerInDict.createKmerDictList(kmerList)
+		aList, tList, gList, cList = splitKmerInDict.getUpdatedCounts(maxKmerLen, kmerDictList);
+		expectedAList = [ 0, 0, 1, 1, 0, 2, 0, 0, 0]
+		expectedTList = [2, 2, 1, 1, 2, 0, 2, 2, 2]
+
+		self.assertTrue(len(set(expectedAList).intersection(aList)) > 0)
+		self.assertTrue(len(set(expectedTList).intersection(tList)) > 0)
+
+
+	def testGetUpdateCountsLargeKmerList(self):
+		kmerList = ['TTTTATTAT', 'TTCTATTTA', 'ATTTATTTT', 'TTAATATTT', 'TTTATTTTT', 'TAATTATTT', 'TTATTTTTA', 'TATTATTTT', 'TATTTATTA', 'CTTATTTAT', 'ATTTTATTT', 'TTTTTATTT', 'TATATTTAT', 'TATTTTATT']
+		maxKmerLen, kmerDictList = splitKmerInDict.createKmerDictList(kmerList)
+		aList, tList, gList, cList = splitKmerInDict.getUpdatedCounts(maxKmerLen, kmerDictList);
+		expectedAList = [2, 5, 3, 4, 4, 5, 1, 3, 3] 
+		expectedTList = [11, 9, 10, 10, 10, 9, 13, 11, 11] 
+		expectedGList = [0, 0, 0, 0, 0, 0, 0, 0, 0] 
+		expectedCList = [1, 0, 1, 0, 0, 0, 0, 0, 0]
+
+		self.assertTrue(len(set(expectedAList).intersection(aList)) > 0)
+		self.assertTrue(len(set(expectedTList).intersection(tList)) > 0)
+		self.assertTrue(len(set(expectedGList).intersection(gList)) > 0)
+		self.assertTrue(len(set(expectedCList).intersection(cList)) > 0)
+
+	def testCreateNormalizedList(self):
+		kmerList = ['TTTTATTAT', 'TTCTATTTA', 'ATTTATTTT', 'TTAATATTT', 'TTTATTTTT', 'TAATTATTT', 'TTATTTTTA', 'TATTATTTT', 'TATTTATTA', 'CTTATTTAT', 'ATTTTATTT', 'TTTTTATTT', 'TATATTTAT', 'TATTTTATT']
+		aList = [2, 5, 3, 4, 4, 5, 1, 3, 3] 
+		tList = [11, 9, 10, 10, 10, 9, 13, 11, 11] 
+		gList = [0, 0, 0, 0, 0, 0, 0, 0, 0] 
+		cList = [1, 0, 1, 0, 0, 0, 0, 0, 0]
+
+		expectedNormalizedAList = [0.143, 0.357, 0.214, 0.286, 0.286, 0.357, 0.071, 0.214, 0.214] 
+		expectedNormalizedTList = [0.786, 0.643, 0.714, 0.714, 0.714, 0.643, 0.929, 0.786, 0.786] 
+		expectedNormalizedGList = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0] 
+		expectedNormalizedCList = [0.071, 0.0, 0.071, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+
+		aList, tList, gList, cList = splitKmerInDict.createNormalizedLists(aList, tList, gList, cList, len(kmerList));
+
+		self.assertTrue(len(set(expectedNormalizedAList).intersection(aList)) > 0)
+		self.assertTrue(len(set(expectedNormalizedTList).intersection(tList)) > 0)
+		self.assertTrue(len(set(expectedNormalizedGList).intersection(gList)) > 0)
+		self.assertTrue(len(set(expectedNormalizedCList).intersection(cList)) > 0)
