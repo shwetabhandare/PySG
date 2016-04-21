65f6ed0c6cedf059b77b207183914c573b31ad3b
Shweta Bhandare <shweta.bhandare@logrhythm.com>
2016-04-21 16:26:09 -0600

    Used matplotlib pyplot.

--- src/parseResults.py
+++ src/parseResults.py
@@ -1,16 +1,73 @@
 import sys
 import os
 import csv
+import matplotlib
+from matplotlib import pyplot as plt
+import numpy as np;
+import math;
 
-def getResultsFromFile(filepath):
+writeTitle = False;
+
+
+def simple():
+    x_data = np.linspace(0., 100., 1000)
+    y_data = ["Signal_100_500_90_100_Cgd2_3490.pwm.fa_dreme", "Signal_100_500_90_100_Gal4.pwm.fa_dreme"]
+    x_data = [0.54, 0.808]
+    for counter, x in enumerate(x_data):
+        y = x_data[counter]
+        matplotlib.pyplot.scatter(x, y)
+
+
+    axes = matplotlib.pyplot.gca()
+    axes.set_xlabel('x')
+    axes.set_ylabel('y')
+    matplotlib.pyplot.show()
+
+def getColumn(filename, column):
+	results = csv.reader(open(filename), delimiter=",")
+	return [result[column] for result in results]
+
+def appendTitle():
+	with open('resultsToGraph.csv', 'wb') as csvfile:	
+		spamwriter = csv.writer(csvfile, delimiter=',');
+		spamwriter.writerow(["label", "sensitivity", "ppv"]);
+
+def appendResult(label, sensitivity, ppv):
+	with open('resultsToGraph.csv', 'a') as csvfile:	
+		spamwriter = csv.writer(csvfile, delimiter=',');
+		spamwriter.writerow([label, sensitivity, ppv])
+
+def getResultsFromFile(filepath, label):
+	global writeTitle;
+
+	if writeTitle == False:
+		appendTitle();
+		writeTitle = True;
+
+
 	with open(filepath, 'rb') as csvfile:
 		reader = csv.reader(csvfile)
 		for row in reader:
 			sensitivity = row[0].split(":")[1]
 			ppv = row[1].split(":")[1]
+			print label, sensitivity, ppv
+			appendResult(label, sensitivity, ppv)
 
-			print sensitivity
-			print ppv
+def graphResults():
+
+	#data1=np.genfromtxt('data1.csv', skip_header=1) 
+	#plt.plot(data1)
+
+
+	label = getColumn("resultsToGraph.csv", 0)
+	sensitivity = getColumn("resultsToGraph.csv", 1)
+	#ppv = getColumn("resultsToGraph.csv", 2)
+
+	plt.figure("sensitivity/ppv")
+	plt.xlabel("filename")
+	plt.ylabel("sensitivity/ppv")
+	plt.plot(label, sensitivity)
+	#plt.plot(label, sensitivity)
 
 
 def parseDirectory(resultDir):
@@ -25,9 +82,11 @@ def parseDirectory(resultDir):
 	            fileNameWithoutPath = os.path.basename(filepath);
 	            fileNameToGraph =  os.path.splitext(fileNameWithoutPath)[0]
 	            print fileNameToGraph
-	            getResultsFromFile(filepath)
+	            getResultsFromFile(filepath, fileNameToGraph)
 
 
 if __name__ == "__main__":
 	resultDir = sys.argv[1]
 	parseDirectory(resultDir)
+	#graphResults();
+	simple();
