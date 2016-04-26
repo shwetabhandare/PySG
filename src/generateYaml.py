0865b99b3d349e8f55f11cba1adab27f2eb7d0c1
Shweta Bhandare <shweta.bhandare@logrhythm.com>
2016-04-26 17:06:57 -0600

    Work in progress for generating conf files using a config yaml.

--- src/generateYaml.py
+++ src/generateYaml.py
@@ -1,11 +1,18 @@
 import yaml
 import os
 from os import path
+import SeqGenUtils
+import shutil;
 
-alpha = [1, 10, 100, 1000]
-numSeq = [1000, 2000]
-seqLen = [500, 600, 700, 800, 900, 1000]
-seqWithSignalPercent = [10, 75, 90]
+alpha = list()
+numSeq = list()
+seqLen = list()
+pwmFiles = list()
+seqLen = []
+seqWithSignalPercent = []
+noSignalType = ""
+signalType = ""
+seqWithSignalPercent = list()
 pwmFileDirectory = "/projects/bhandare/workspace/PySG/data/pwm"
 utrDist = dict(
 	A = 0.27,
@@ -91,7 +98,7 @@ def writeYamlFile(location, yamlFileName, noSignalDict, motifDict):
 		 outfile.write(yaml.dump(data, default_flow_style=False, Dumper=noalias_dumper))
 
 def getPwmFiles(pwmDir):
-	pwmFiles = [];
+	global pwmFiles;
 	for f in os.listdir(pwmDir):
 		if f.lower().endswith(('.pwm')):
 			pwmFiles.append(f)
@@ -131,14 +138,104 @@ def GenerateNoSignalWithFasta(location):
 					writeYamlFile(location, yamlFileName, noSignalDict, motifDict);
 
 ## Program stats here.
-def CreateConfFiles(location, type):
-	if type == 'shuffle':
-		GenerateNoSignalWithFasta(location);
-	elif type == 'dirichlet':
-		GenerateNoSignalWithDirichlet(location);
+def CreateConfFiles(targetDir, noSignalType):
+	print "NO SIGNAL TYPE:", noSignalType
+	if noSignalType == 'shuffle':
+		print "Shuffle type";
+		#GenerateNoSignalWithFasta(targetDir);
+	elif noSignalType == 'dirichlet':
+		print "dirichlet"
+		GenerateNoSignalWithDirichlet(targetDir);
+	else:
+		print "Invalid No Signal type", noSignalType;
+
+def setupNoSignalVariables(confMap):
+	global noSignalType, alpha, noSignalFastaFile;
+	if confMap["nosignal"].get("type"):
+		noSignalType = confMap["nosignal"]["type"]
+		if noSignalType == "dirichlet":
+			alpha = confMap["nosignal"]["alpha"]
+		elif noSignalType == "shuffle":
+			noSignalFastaFile = confMap["nosignal"]["fastaFile"]
+		else:
+			print "Did not specify nosignal type"
+			return;
+	return noSignalType;
+
+def getSignalType(confMap):
+	global signalType;
+
+	if confMap["signal"].get("type"):
+		signalType = confMap["signal"]["type"]
+		if signalType == 'pwmFiles':
+			pwmFiles = confMap["signal"]["pwmFiles"]
+		elif signalType == "pwmDir":
+			pwmDir =  confMap["signal"]["pwmDir"]
+			pwmFiles = getPwmFiles(pwmDir);
+		elif signalType == "textMotif":
+			textMotifs = confMap["signal"]["textMotif"]
+		elif signalType == "kmers":
+			kmers = confMap["signal"]["kmers"]
+		else:
+			print "Invalid signal Type:", signalType;
+	return signalType;
+
+def setupSignalVariables(confMap):
+	global signalType, signalLocation, seqWithSignalPercent, pwmFiles;
+
+	signalType = getSignalType(confMap)
+
+	if confMap["signal"].get("location"):
+		signalLocation = confMap["signal"]["location"]
+	else:
+		signalLocation = [0]
+	
+	if confMap["signal"].get("signalPercent"):
+		seqWithSignalPercent = confMap["signal"]["signalPercent"]		
+	else:
+		seqWithSignalPercent = [100]
+
+
+	return signalType;
+
+
+def setupVariables(confMap):
+	global numSeq, seqLen;
+	numSeq = confMap["numSeq"]
+	seqLen = confMap["seqLen"]
+	signalType = setupSignalVariables(confMap)
+	noSignalType = setupNoSignalVariables(confMap)
+	return signalType, noSignalType;
+
+def deleteFiles(targetDir):
+	for the_file in os.listdir(targetDir):
+		file_path = os.path.join(targetDir, the_file)
+		try:
+			if os.path.isfile(file_path):
+				os.unlink(file_path)
+			elif os.path.isdir(file_path): 
+				shutil.rmtree(file_path)
+		except Exception as e:
+			print(e)
+
+def getTargetDir(confMap):
+	if confMap.get("targetDir"):
+		targetDir = confMap["targetDir"]
+	else:
+		targetDir = os.os.getcwd() + "/" + "tmp";
+
+	if not os.path.exists(targetDir):
+		print "Creating directory: ", targetDir
+		os.makedirs(targetDir)
+	else:
+		deleteFiles(targetDir)
+
+	return targetDir;
 
 if __name__ == "__main__":
 	import sys
-	location = sys.argv[1]
-	type = sys.argv[2]
-	CreateConfFiles(location, type)
+	confFile = sys.argv[1]
+	confMap = SeqGenUtils.GetConf(confFile)
+	targetDir = getTargetDir(confMap)
+	signalType, noSignalType = setupVariables(confMap)
+	CreateConfFiles(targetDir, noSignalType)
--- src/test_generate_yaml.py
+++ src/test_generate_yaml.py
@@ -1,36 +1,30 @@
+import unittest
 import generateYaml
 import glob
 import os
+import yaml
+import SeqGenUtils
 
-class TestGenerateYaml:
-	def test_conf(self):
-		generateYaml.CreateConfFiles("/tmp");
-		files = glob.glob("/tmp/*.yml")
-		assert len(files) > 0
+class TestGenerateYaml(unittest.TestCase):
+	def test_getTargetDir(self):
+		confFile = "/projects/bhandare/workspace/PySG/src/resources/experiments.yaml"
+		confMap = SeqGenUtils.GetConf(confFile);
+		targetDir = generateYaml.getTargetDir(confMap);
+		self.assertEqual(targetDir, "/projects/bhandare/workspace/PySG/src/resources/tmp")
+		self.assertTrue(os.path.isdir(targetDir))
 
-	def test_generate_yaml_hur(self):
-		generateYaml.generateYaml("100", "/tmp", 10, 100, 10, 20, 30, 10, 40, "HuR", "ATTTA", 1, 0);
-		expectedContent = "{motif: {distance: 0, length: 5, location: random, motif: ATTTA, numMotifs: 1, type: HuR}, sequence: {A: 20, C: 40, G: 10, T: 30, maxLen: 100, minLen: 10, numSeq: 10}}"
-		assert os.path.isfile('/tmp/seq_100.yml');
-		content = ""
-		with open('/tmp/seq_100.yml', 'r') as content_file:
-			content = " ".join(line.strip() for line in content_file);
-    		assert expectedContent.strip() == content.strip();
+	def test_setupVariables(self):
+		confFile = "/projects/bhandare/workspace/PySG/src/resources/experiments.yaml"
+		confMap = SeqGenUtils.GetConf(confFile);
+		targetDir = generateYaml.getTargetDir(confMap);	
+		signalType, noSignalType = generateYaml.setupVariables(confMap)
+		self.assertEqual(signalType, "pwmFiles");
+		self.assertEqual(noSignalType, "dirichlet");
 
-	def test_generate_yaml_ttp(self):
-		generateYaml.generateYaml("200", "/tmp", 10, 100, 10, 20, 30, 10, 40, "TTP", "TTATTTATT", 1, 0);
-		expectedContent = "{motif: {distance: 0, length: 9, location: random, motif: TTATTTATT, numMotifs: 1, type: TTP}, sequence: {A: 20, C: 40, G: 10, T: 30, maxLen: 100, minLen: 10, numSeq: 10}}"
-		assert os.path.isfile('/tmp/seq_200.yml');
-		content = ""
-		with open('/tmp/seq_200.yml', 'r') as content_file:
-			content = " ".join(line.strip() for line in content_file);
-    		assert expectedContent.strip() == content.strip();   	
-
- 	def test_generate_yaml_generated(self):
-		generateYaml.generateYaml("300", "/tmp", 10, 100, 10, 20, 30, 10, 40, "Generated", "AAATTTGGGCCC", 1, 0);
-		expectedContent = "{motif: {distance: 0, length: 12, location: random, motif: AAATTTGGGCCC, numMotifs: 1, type: Generated}, sequence: {A: 20, C: 40, G: 10, T: 30, maxLen: 100, minLen: 10, numSeq: 10}}"
-		assert os.path.isfile('/tmp/seq_300.yml');
-		content = ""
-		with open('/tmp/seq_300.yml', 'r') as content_file:
-			content = " ".join(line.strip() for line in content_file);
-    		assert expectedContent.strip() == content.strip();   	s
+	def test_setupVariables_PwmFileDir(self):
+		confFile = "/projects/bhandare/workspace/PySG/src/resources/experiments1.yaml"
+		confMap = SeqGenUtils.GetConf(confFile);
+		targetDir = generateYaml.getTargetDir(confMap);	
+		signalType, noSignalType = generateYaml.setupVariables(confMap)
+		self.assertEqual(signalType, "pwmDir");
+		self.assertEqual(noSignalType, "shuffle");
